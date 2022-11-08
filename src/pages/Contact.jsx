efa9f85ad8b49097299bc7ff440c2b328055ba8c
ajaysinghpanwar2002 <ajaysingh07032003@gmail.com>
2022-11-08 08:00:10 +0530

    contact page completed

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React from 'react';
+import SocialCard from '../components/SocialCard';
+import { Grid, Box } from '@mui/material'
+import contact from '../assets/contact.jpg'
+import '../styles/Contact.css'
+import Contactdata from '../data/Contactdata';
 
-function Contact() {
+const Contact = () => {
+
+  var SocialCardGrid = Contactdata.map((site, ind) => {
+      return (
+          <Grid item lg={4} md={4} xs={6} className="item">
+              <SocialCard item
+                  id={ind}
+                  {...site}
+              />
+          </Grid>
+      )
+  })
+
+
   return (
-    <div>Contact</div>
+      <Box className="contactSection">
+          <Grid container className="contactGrid" spacing={2} >
+              <Grid item lg={6} md={12} xs={12} className="leftCol">
+                  <img src={contact} className="bannerImage" style={{ height: '480px' }} />
+              </Grid>
+              <Grid item lg={6} md={12} xs={12} className="rightCol">
+                  {SocialCardGrid}
+              </Grid>
+          </Grid>
+      </Box>
   )
 }
 
