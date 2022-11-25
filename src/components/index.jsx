647d635a2a08db231954f1bf04de26b6599b3832
ajaysinghpanwar2002 <ajaysingh07032003@gmail.com>
2022-11-25 22:11:03 +0530

    dark mode toggle, minor changes

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -3,5 +3,6 @@ import Navbar from "./Navbar";
 import MobileNavabar from "./MobileNavabar";
 import Globe from "./customGlobe";
 import { GoogleLogo } from "./googleLogo";
+import ToggleButtons from "./ToggleButtons"
 
-export { Navbar, Footer, Globe, GoogleLogo,MobileNavabar };
+export { Navbar, Footer, Globe, GoogleLogo,MobileNavabar,ToggleButtons};
