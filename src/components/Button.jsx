60aa12f555fa5371f28617ca4791862e5b957bc7
ajaysinghpanwar2002 <ajaysingh07032003@gmail.com>
2023-02-28 22:49:14 +0530

    fixes

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,9 +4,9 @@ import '../styles/Button.css';
 export default function Button(props) {
     return (
         <button className="btn"
-            // style={{background:`${props.color}`}}
-            style={props.css}
-            onClick={(e) => { console.log('button is clicked') }}
+            style={{background:`${props.color}`}}
+            // style={props.css}
+            // onClick={(e) => { console.log('button is clicked') }}
         >
             {props.text}
         </button>);
--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import "../styles/components/InputForm.css"
 import inputformimage from "../assets/inputFormImage.svg"
+import ideaicon from '../assets/idea-icon.svg'
 import emailjs from "emailjs-com";
 function InputForm() {
     function sendEmail(e) {
@@ -9,15 +10,17 @@ function InputForm() {
             {
                 console.log(res);
             }).catch(err=> console.log(err));
+            alert('message sent');
     }
     return (
         <>
             <form className='home-input-form' onSubmit={sendEmail}>
+                <div className='project-submission'>Submit Your Project Ideas</div>
                 <input type="text" placeholder="Enter your name" name="name" className='home-input-form-name' />
                 <input type="email" placeholder='Enter your email' name='userEmail' className='home-input-form-name'/>
                 <input type="text" placeholder="Your Query" name='message' className='home-input-form-message' />
                 <div className='home-bottom-input-form'>
-                    {/* <img src={inputformimage} alt="mailus" className='input-form-image' /> */}
+                    <img src={ideaicon} alt="mailus" className='input-form-image' />
                     <button className="button-home-input-form" type='submit' value="Send">
                         submit
                     </button>
